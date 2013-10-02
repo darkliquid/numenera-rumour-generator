3f8e3cc473ae0fa6ff30170938e23d14afe9aa5a
Andrew Montgomery-Hurrell <andrew@multiplay.co.uk>
2013-10-02 17:45:00 +0100

    Numenera rumour generator commit

---
+++ app.js
@@ -0,0 +1,51 @@
+function randomArrayElement(arr) {
+  return arr[Math.floor(Math.random()*arr.length)];
+}
+
+function capitaliseFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+    
+function tablestuff(data, tabletop) {
+  var button = document.getElementById('generate'),
+      rumour = document.getElementById('rumour');
+          
+  function generate() {
+    var phrase = randomArrayElement(data.Phrase.elements).phrase,
+        aside = randomArrayElement(data.Aside.elements).aside,
+        replacer = function(match, key) {
+          if(key) {
+            var column = key.toLowerCase(), row;
+            if(key.indexOf('-') > -1) {
+              var tmp = key.split('-');
+              key = tmp[0];
+              row = randomArrayElement(data[key].elements);
+              var result = row[tmp[1].toLowerCase()];
+              if(result && result.length > 0) {
+                return result;
+              } else {
+                column = key.toLowerCase();
+              }
+            } else {
+              row = randomArrayElement(data[key].elements);
+            }
+            return row[column];
+          } else {
+            return 'ERROR';
+          }
+        };
+     
+    var p = phrase.replace(/\%([A-Za-z\-]+)\%/g, replacer);
+    var a = aside.replace(/\%([A-Za-z\-]+)\%/g, replacer);
+    rumour.innerHTML = capitaliseFirstLetter(p) + '. ' + a + '.';
+  };
+  
+  button.onclick = generate;
+  generate();
+}
+    
+Tabletop.init( { 
+  key: '0AuER3PNq5JgBdEsxTlJIZjhpRHJqZkJudkE0eUpEM1E',
+  callback: tablestuff,
+  simpleSheet: false
+});
